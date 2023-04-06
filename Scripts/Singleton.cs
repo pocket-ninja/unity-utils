26477c6f84d901c0e3b57b47adddc1baf331e13c
sroik <vasili.kazhanouski@gmail.com>
2023-04-06 13:58:18 +0300

    singleton

---
+++ Scripts/Singleton.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+namespace PocketUtils {
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T> {
+        private static T _instance;
+
+        public static T instance {
+            get {
+                if (_instance) {
+                    return _instance;
+                }
+
+                var objects = FindObjectsOfType<T>();
+
+                if (objects.Length == 0) {
+                    var gameObject = new GameObject($"[{TypeName}]");
+                    DontDestroyOnLoad(gameObject);
+                    var instance = gameObject.AddComponent<T>();
+                    instance.Setup();
+                    _instance = instance;
+                    return _instance;
+                }
+
+                if (objects.Length > 1) {
+                    Debug.LogError($"There are more than one instance of {TypeName}!");
+                }
+
+                _instance = objects[0];
+                return _instance;
+            }
+        }
+
+        private static string TypeName {
+            get {
+                return typeof(T).Name;
+            }
+        }
+
+        public virtual void Setup() {
+            /// It's meant to be overriden
+        }
+    }
+}
