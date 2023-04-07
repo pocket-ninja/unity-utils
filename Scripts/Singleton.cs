82e6f4489e0c05b76e4afa95c94b71572684bbb0
sroik <vasili.kazhanouski@gmail.com>
2023-04-07 15:46:53 +0300

    Update Singleton.cs

--- Scripts/Singleton.cs
+++ Scripts/Singleton.cs
@@ -5,7 +5,7 @@ namespace PocketApps.Utils {
     public class Singleton<T> : MonoBehaviour where T : Singleton<T> {
         private static T _instance;
 
-        public static T instance {
+        public static T Instance {
             get {
                 if (_instance) {
                     return _instance;
