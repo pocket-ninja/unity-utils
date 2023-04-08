8d35b19a81f5ccd45531179d467d1b90508e1d6d
sroik <vasili.kazhanouski@gmail.com>
2023-04-08 20:21:17 +0300

    Update SafeArea.cs

--- Scripts/SafeArea.cs
+++ Scripts/SafeArea.cs
@@ -1,35 +1,35 @@
 using UnityEngine;
 
 namespace PocketApps.Utils {
+    [ExecuteAlways]
     public class SafeArea : MonoBehaviour {
 
         [SerializeField]
         private RectTransform _target;
 
-        private Rect _recentSafeArea = new Rect(0, 0, 0, 0);
+        [SerializeField]
+        private bool _logsEnabled;
 
-        private void Awake() {
-            Layout();
-        }
+        private Rect _recentSafeArea = Rect.zero;
 
         private void Start() {
             Layout();
         }
 
-        private void FixedUpdate() {
+        private void Update() {
             Layout();
         }
 
         private void Layout() {
             Rect safeArea = Screen.safeArea;
 
-            if (_recentSafeArea == safeArea) {
+            if (_recentSafeArea == safeArea && Application.isPlaying) {
                 return;
             };
 
             _recentSafeArea = safeArea;
 
-            /// Let's convert safe area rect from absolute pixels to normalized anchor coordinates
+            /* Let's convert safe area rect from absolute pixels to normalized anchor coordinates */
             Vector2 anchorMin = safeArea.position;
             Vector2 anchorMax = safeArea.position + safeArea.size;
             anchorMin.x /= Screen.width;
@@ -39,16 +39,18 @@ namespace PocketApps.Utils {
             _target.anchorMin = anchorMin;
             _target.anchorMax = anchorMax;
 
-            Debug.LogFormat(
-                "New safe area applied to {0}: x={1}, y={2}, w={3}, h={4} on full extents w={5}, h={6}",
-                name,
-                safeArea.x,
-                safeArea.y,
-                safeArea.width,
-                safeArea.height,
-                Screen.width,
-                Screen.height
-            );
+            if (_logsEnabled) {
+                Debug.LogFormat(
+                    "New safe area applied to {0}: x={1}, y={2}, w={3}, h={4} on full extents w={5}, h={6}",
+                    name,
+                    safeArea.x,
+                    safeArea.y,
+                    safeArea.width,
+                    safeArea.height,
+                    Screen.width,
+                    Screen.height
+                );
+            }
         }
     }
 }
