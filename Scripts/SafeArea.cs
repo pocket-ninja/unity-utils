ca972b2e48933c971e04df773a810b85e9fd8fb0
sroik <vasili.kazhanouski@gmail.com>
2023-04-08 17:41:57 +0300

    Update SafeArea.cs

--- Scripts/SafeArea.cs
+++ Scripts/SafeArea.cs
@@ -43,11 +43,11 @@ namespace PocketApps.Utils {
                 "New safe area applied to {0}: x={1}, y={2}, w={3}, h={4} on full extents w={5}, h={6}",
                 name,
                 safeArea.x,
-                 safeArea.y,
-                 safeArea.width,
-                 safeArea.height,
-                 Screen.width,
-                 Screen.height
+                safeArea.y,
+                safeArea.width,
+                safeArea.height,
+                Screen.width,
+                Screen.height
             );
         }
     }
