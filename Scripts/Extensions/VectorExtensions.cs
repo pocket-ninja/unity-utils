430ae878dfc77ac6965baf3c74d31dd265db450d
sroik <vasili.kazhanouski@gmail.com>
2023-04-17 11:58:20 +0300

    Update VectorExtensions.cs

--- Scripts/Extensions/VectorExtensions.cs
+++ Scripts/Extensions/VectorExtensions.cs
@@ -13,5 +13,24 @@ namespace PocketApps.Utils {
         public static Vector2 ToXZ(this Vector3 v3) {
             return new Vector2(v3.x, v3.z);
         }
+        
+        /// <summary> Change Vector3's specific coordinate values </summary>
+        public static Vector3 With(this Vector3 v3, float? x = null, float? y = null, float? z = null) {
+            var result = v3;
+
+            if (x.HasValue) {
+                result.x = x.Value;
+            }
+
+            if (y.HasValue) {
+                result.y = y.Value;
+            }
+
+            if (z.HasValue) {
+                result.z = z.Value;
+            }
+
+            return result;
+        }
     }
 }
